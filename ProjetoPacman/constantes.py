804e75a21b571bbd0c2f4faee38028b3123ad4d9
NillDev <68431145+Nilldev@users.noreply.github.com>
2023-05-09 11:59:18 -0300

    Pacman

---
+++ ProjetoPacman/constantes.py
@@ -0,0 +1,24 @@
+
+#dimensões da tela
+
+LARGURA = 480
+ALTURA = 600
+
+#titulo do jogo
+
+TITULO_JOGO = 'PACMAN'
+
+#fps
+FPS =30
+
+#cores
+PRETO = (0,0,0)
+BRANCO = (255,255,255)
+AMARELO = (244,233,51)
+
+#imagens
+SPRITESHEET = 'Spritesheet.png'
+PACMAN_START_LOGO = 'pacman_logo-1.png'
+
+#fonte
+FONTE =  'arial'
